ebe3601c2668c426725a85c520255c8f11bfba26
Andrew Zbrutskii <zbrutskii@gmail.com>
2017-11-04 17:08:48 +0200

    Footer added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 jQuery(document).ready( function($) {
 
+    // Mobile nav
+
     $('.header--nav-toggle').click( function() {
         $(this).toggleClass('open');
         $('.header--mobile-box').toggleClass('show');
@@ -11,5 +13,58 @@ jQuery(document).ready( function($) {
         $('.header--filter').removeClass('show');
         $('.header--nav-toggle').removeClass('open');
     });
+
+
+    // Autohide header
+
+    var mainHeader = $('header'),
+        headerHeight = mainHeader.height();
+
+    var scrolling = false,
+        previousTop = 0,
+        currentTop = 0,
+        scrollDelta = 10,
+        scrollOffset = 150;
+
+    mainHeader.on('click', '.nav-trigger', function(event){
+        // open primary navigation on mobile
+        event.preventDefault();
+        mainHeader.toggleClass('nav-open');
+    });
+
+    $(window).on('scroll', function(){
+        if( !scrolling ) {
+            scrolling = true;
+            (!window.requestAnimationFrame)
+                ? setTimeout(autoHideHeader, 250)
+                : requestAnimationFrame(autoHideHeader);
+        }
+    });
+
+    $(window).on('resize', function(){
+        headerHeight = mainHeader.height();
+    });
+
+    function autoHideHeader() {
+        var currentTop = $(window).scrollTop();
+
+        ( belowNavHeroContent.length > 0 )
+            ? checkStickyNavigation(currentTop) // secondary navigation below intro
+            : checkSimpleNavigation(currentTop);
+
+        previousTop = currentTop;
+        scrolling = false;
+    }
+
+    function checkSimpleNavigation(currentTop) {
+        //there's no secondary nav or secondary nav is below primary nav
+        if (previousTop - currentTop > scrollDelta) {
+            //if scrolling up...
+            mainHeader.removeClass('is-hidden');
+        } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+            //if scrolling down...
+            mainHeader.addClass('is-hidden');
+        }
+    }
 
 });
