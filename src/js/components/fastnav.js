420f66683224d068be27dff3d69373030ba02b6a
Alexander <dev@salex.pro>
2018-05-27 02:30:58 +0300

    new main page

--- src/js/components/fastnav.js
+++ src/js/components/fastnav.js
@@ -11,4 +11,4 @@ const pageWidget = pages => {
     });
 }
 
-pageWidget(['index', 'new', 'analytics', 'advertising', 'lead_magnet', 'native_advertising_content', 'thank_you']);
+pageWidget(['index', 'analytics', 'advertising', 'lead_magnet', 'native_advertising_content', 'thank_you']);
