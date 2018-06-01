f53a65975ca89d8a4d8f3cc06676a45fbc0a7f11
Alexander <dev@salex.pro>
2018-06-01 14:44:09 +0300

    restructure some blocks & interactive treemap

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
+/* 
+@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.slim.min'
+@codekit-prepend quiet '../../node_modules/wow.js/dist/wow.min'
+*/
+
 jQuery(document).ready( function($) {
 
     // Mobile nav
@@ -73,4 +78,23 @@ jQuery(document).ready( function($) {
         }
     }
 
+});
+
+$(document).ready(function () {
+    // Initialise WOW
+    new WOW().init();
+
+    // Scroll to hash
+    $('a.scroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
 });
